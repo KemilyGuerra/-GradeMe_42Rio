b0441aa6eb06f92c53572e8586fb4e2c3e8265b9
kemily dos santos <kemilyguerr@gmail.com>
2025-03-07 00:33:30 +0000

    al

--- rendu/maff_revalpha/maff_revalpha.c
+++ rendu/maff_revalpha/maff_revalpha.c
@@ -1,6 +1,14 @@
 #include <unistd.h>
 int main()
 {
-	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+	int i;
+	i = 0;
+	char str[] = "zyxwvutsrqponmlkjihgfedcba";
+
+	while(str[i] >= 'a' && str[i] <= 'z') 
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 	return (0);
 }
